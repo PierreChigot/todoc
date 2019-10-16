31d96468b0450c5a7a61a0984c9aefa828309e28
pierre <pierre.chigot@gmail.com>
2019-10-16 19:36:12 +0200

    après mentoring

--- app/src/main/java/com/cleanup/todoc/ui/MainViewModel.java
+++ app/src/main/java/com/cleanup/todoc/ui/MainViewModel.java
@@ -1,10 +1,13 @@
 package com.cleanup.todoc.ui;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.cleanup.todoc.data.ProjectDao;
@@ -26,72 +29,127 @@ public class MainViewModel extends ViewModel {
     private TaskDao mTaskDao;
 
 
-
-    private final MutableLiveData<List<TaskUIModel>> mUiModelsLiveData = new MutableLiveData<>();
+    private final MediatorLiveData<List<TaskUIModel>> mUiModelsLiveData = new MediatorLiveData<>();
     private final SingleLiveEvent<ViewAction> mSingleLiveDataEvent = new SingleLiveEvent<>();
 
 
-    int mSortMethod = -1;
+
+    int mSortMethod = 1;
 
 
     public MainViewModel(@NonNull ProjectDao projectDao, @NonNull TaskDao taskDao) {
         mProjectDao = projectDao;
         mTaskDao = taskDao;
-
+        wireUpMediator();
 
 
     }
+
+    private void wireUpMediator() {
+        final LiveData<List<Project>> liveDataProject = mProjectDao.getProjectsLiveData();
+        final LiveData<List<Task>>liveDataTasks = mTaskDao.getTasksLiveData();
+
+        mUiModelsLiveData.addSource(liveDataProject, new Observer<List<Project>>() {
+            @Override
+            public void onChanged(List<Project> projects) {
+                combineProjectsAndTasks(projects,liveDataTasks.getValue());
+            }
+        });
+        mUiModelsLiveData.addSource(liveDataTasks, new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                combineProjectsAndTasks(liveDataProject.getValue(), tasks);
+            }
+        });
+    }
+
+
     LiveData<List<TaskUIModel>> getUiModelsLiveData() {
         return mUiModelsLiveData;
     }
+
     LiveData<ViewAction> getViewActionMutableLiveData() {
         return mSingleLiveDataEvent;
     }
 
-    void refresh(){
-        //TODO on doit observer ces live data : ???
-        final LiveData<List<Project>> projectLiveData = mProjectDao.getProjectsLiveData();
-        final LiveData<List<Task>> tasksLiveData = mTaskDao.getTasksLiveData();
+    void combineProjectsAndTasks(List<Project> projects, List<Task> tasks) {
+
         List<TaskUIModel> uiModels = new ArrayList<>();
 
-        //TODO comment savoir si la livedata est empty?
-        //TODO repository or not repository ???
-        //TODO créer un mock ou remplir la DB si la Table Project est vide ?
+        Log.d("Pierre", "combineProjectsAndTasks() called with: projects = [" + projects + "], tasks = [" + tasks + "]");
 
-        List<Task> updatedTasks ;
-        if (tasksLiveData == null){
-            mSingleLiveDataEvent.setValue(ViewAction.NO_TASK);
-        }else {
-            updatedTasks = tasksLiveData.getValue();
-            if (mSortMethod == 1){
+        //TODO remplir la Table Project est vide
+        //TODO singleton in AppDatabase ?
+
+       /* if (projectLiveData == null) {
+            Project project1 = new Project(1L, "Projet Tartampion", 0xFFEADAD1);
+            Project project2 = new Project(2L, "Projet Lucidia", 0xFFB4CDBA);
+            Project project3 = new Project(3L, "Projet Circus", 0xFFA3CED2);
+            addProject(project1);
+            addProject(project2);
+            addProject(project3);
+        }*/
+
+
+       /* if (tasksLiveData == null) {
+
+        } else {*/
+       if (tasks == null || tasks.isEmpty()) {
+           mSingleLiveDataEvent.setValue(ViewAction.NO_TASK);
+           return;
+       }
+       if (projects == null || projects.isEmpty()) {
+           mSingleLiveDataEvent.setValue(ViewAction.NO_TASK);
+           return;
+       }
+        for (Project project : projects) {
+            for (Task task : tasks) {
+                if (project.getId() == task.getProjectId()){
+                    uiModels.add(map(project.getId(), task.getName(), project.getColor()));
+                }
+
+            }
+        }
+            /*updatedTasks = tasksLiveData.getValue();
+            if (mSortMethod == 1) {
                 Collections.sort(updatedTasks, new Task.TaskAZComparator());
-            }else if (mSortMethod == 2){
+            } else if (mSortMethod == 2) {
                 Collections.sort(updatedTasks, new Task.TaskZAComparator());
-            }else if (mSortMethod == 3){
+            } else if (mSortMethod == 3) {
                 Collections.sort(updatedTasks, new Task.TaskOldComparator());
-            }else if (mSortMethod == 4){
+            } else if (mSortMethod == 4) {
                 Collections.sort(updatedTasks, new Task.TaskRecentComparator());
             }
             for (Task updatedTask : updatedTasks) {
                 int colorRes = -1;
 
-                if (updatedTask.getProjectId() == 1L){
+                if (updatedTask.getProjectId() == 1L) {
                     colorRes = 0xFFEADAD1;
-                }else if (updatedTask.getProjectId() == 2L){
+                } else if (updatedTask.getProjectId() == 2L) {
                     colorRes = 0xFFB4CDBA;
-                }else if (updatedTask.getProjectId() == 3L) {
+                } else if (updatedTask.getProjectId() == 3L) {
                     colorRes = 0xFFA3CED2;
                 }
                 uiModels.add(new TaskUIModel(updatedTask.getId(), updatedTask.getName(), colorRes));
             }
-        }
+        */
         mUiModelsLiveData.setValue(uiModels);
     }
 
-    void  addTask(Task task){
+    private TaskUIModel map(long id, String name, int color) {
 
-        new  InsertDataAsyncTask(mProjectDao, mTaskDao, task).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        return new TaskUIModel(id,name,color);
+    }
 
+    void addTask(Task task) {
+
+        new InsertDataAsyncTask(mProjectDao, mTaskDao, task, null).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+    }
+
+    void addProject(Project project) {
+
+        new InsertDataAsyncTask(mProjectDao, mTaskDao, null, project).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     void deleteTask(long taskId) {
@@ -108,18 +166,21 @@ public class MainViewModel extends ViewModel {
 
         private final Task mTask;
 
-        private InsertDataAsyncTask(@NonNull ProjectDao projectDao, @NonNull TaskDao taskDao, Task task) {
-           mProjectDao = projectDao;
-           mTaskDao = taskDao;
-           mTask = task;
+        private final Project mProject;
 
+        private InsertDataAsyncTask(@NonNull ProjectDao projectDao, @NonNull TaskDao taskDao, Task task, Project project) {
+            mProjectDao = projectDao;
+            mTaskDao = taskDao;
+            mTask = task;
+            mProject = project;
         }
 
 
         @Override
         protected Void doInBackground(Void... voids) {
+            mProjectDao.createProject(new Project(1L, "Projet Tartampion", 0xFFEADAD1));
+            mTaskDao.insertTask(mTask);
 
-           mTaskDao.insertTask(mTask);
 
             return null;
         }
